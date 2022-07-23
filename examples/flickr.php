7f091f3bc4c74a9e17a32f4cfb9ce967e4e0d137
Pascal Canadas <pascal@mirandais.ovh>
2022-07-23 21:59:16 +0200

    🎨 Mise à jour des dépots

--- examples/flickr.php
+++ examples/flickr.php
@@ -1,26 +1,19 @@
 <?php
 
 require '../vendor/autoload.php';
-// $cacher = new Doctrine\Common\Cache\ArrayCache();
-$cacher = new Doctrine\Common\Cache\FilesystemCache('/tmp');
+
+
 
 $uploader = RemoteImageUploader\Factory::create('Flickr', array(
-    'cacher'         => $cacher,
-    'api_key'        => 'your api key',
-    'api_secret'     => 'your api secret',
-
-    // if you have oauth_token and secret, you can set
-    // to the options to pass
-    'oauth_token'        => null,
-    'oauth_token_secret' => null,
+    'api_key'        => '',
+    'api_secret'     => '',
+    'oauth_token'        => '',
+    'oauth_token_secret' => '',
 ));
 
-$callbackUrl = 'http'.(getenv('HTTPS') == 'on' ? 's' : '').'://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+$callbackUrl = 'http:\\127.0.0.1';
 
 $uploader->authorize($callbackUrl);
 
-$url = $uploader->upload('/Volumes/Data/Data/Photos/My Icon/ninja.JPG');
-var_dump($url);
-
-$url = $uploader->transload('http://s26.postimg.org/f0lrm6vqh/ninja.jpg');
+$url = $uploader->upload('708669.jpg');
 var_dump($url);
